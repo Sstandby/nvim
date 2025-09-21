8937f2c9604e9cae1749a58a0622975df9543389
Sstandby <standbyy9@gmail.com>
2025-09-21 01:08:45 -0500

    refactor: re size buffers

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -116,10 +116,10 @@ keymap.set("n", "s<down>", "<C-w>j", opts)
 keymap.set("n", "s<right>", "<C-w>l", opts)
 
 -- Resize window
-keymap.set("n", "<C-w><left>", "<C-w><")
-keymap.set("n", "<C-w><right>", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
+keymap.set("n", "<C-w><right>", "<C-w><")
+keymap.set("n", "<C-w><left>", "<C-w>>")
+keymap.set("n", "<C-w><down>", "<C-w>+")
+keymap.set("n", "<C-w><up>", "<C-w>-")
 
 --  Tree
 keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", opts)
